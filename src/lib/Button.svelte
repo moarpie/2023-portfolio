5d53f998d6450cc22eb80c65c54b1e6f244ac0f2
mje <mje@unik.dk>
2023-02-17 14:44:16 +0100

    Yeeted unneeded default Svelte stuff

---
+++ src/lib/Button.svelte
@@ -0,0 +1,30 @@
+<script>
+    export let label;
+    export let link;
+</script>
+
+<a class="button {$$props.class}" href="{link}">{label}</a>
+
+<style lang="scss">
+    /*strips all standard link styling*/
+    a {
+        all: unset;
+        display: block;
+        &:hover {
+            background-color: red;
+        }
+    }
+
+
+    .button {
+        background: #330066;
+        color: white;
+        padding: 16px;
+    }
+    .small {
+        height: 20px;
+    }
+    .big {
+        height: 50px;
+    }
+</style>
