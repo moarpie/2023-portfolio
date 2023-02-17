a297ba9f00bfd9872765e2069a002d2fddd40663
mje <mje@unik.dk>
2023-02-17 16:15:29 +0100

    Successfully implemented feature to ingest tokens from Token Studio, transform them with token-transformer, and build scss variables with style-dictionary

---
+++ build-tokens.js
@@ -0,0 +1,57 @@
+const { transformTokens } = require('token-transformer');
+const styleDictionary = require('style-dictionary');
+const fs = require('fs/promises');
+
+//load original tokens file from Token Studio
+function getFigmaTokens() {
+    return fs.readFile('tokens/tokens.json', { encoding: 'utf8', flag: 'r' })
+        .then(fileContent => JSON.parse(fileContent));
+}
+
+function resolveTokens(rawTokens) {
+	//inputs for token-transformer
+	const setsToUse = ['global','button'];
+	const excludes = ['global'];
+	//settings for token-transformer
+	const transformerOptions = {
+		expandTypography: true,
+		expandShadow: true,
+		expandComposition: true,
+		preserveRawValue: false,
+		throwErrorWhenNotResolved:  true,
+		resolveReferences:true
+	}
+
+	return resolvedTokens = transformTokens(rawTokens,setsToUse, excludes,transformerOptions);
+
+}
+
+
+function styleDictionaryBuilder(inputTokens) {
+    styleDictionary.extend({
+        'tokens': inputTokens,
+        'platforms': {
+            'scss': {
+                'transformGroup': 'scss',
+                'files': [
+                    {
+                        'destination': `src/css/_variables.scss`,
+                        'format': 'scss/variables',
+                        'options': {
+                            'outputReferences': true
+                        }
+                    }
+                ]
+            }
+        }
+    }).buildAllPlatforms();
+}
+
+async function generateVars() {
+	const rawTokens = await getFigmaTokens();
+	const transformedTokens = await resolveTokens(rawTokens);
+	console.log(transformedTokens);
+	styleDictionaryBuilder(transformedTokens);
+	
+}
+generateVars();
