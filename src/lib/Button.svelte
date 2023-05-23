2b0fe6330744a88f3daeb3a0bbad2059dc24e5db
mje <mje@unik.dk>
2023-05-23 23:48:17 +0200

    Recreated button

---
+++ src/lib/Button.svelte
@@ -0,0 +1,53 @@
+<script>
+    import { createEventDispatcher } from 'svelte';
+
+    const dispatch = createEventDispatcher();
+    function handleClick() {
+        dispatch('click');
+    }
+    export let label;
+    export let link;
+</script>
+
+<a class="button {$$props.class}" on:click={handleClick} href="{link}">{label}</a>
+
+<style>
+   /* @import "../css/vars/_variables.scss"; */
+    /*strips all standard link styling*/
+    a {
+        all: unset;
+        display: inline-flex;
+        box-sizing: border-box;
+        align-items: center;
+        justify-content: center;
+        cursor: pointer;
+
+    }
+    .button {
+        text-align: center;
+        transition: background-color 300ms;
+        background: var(--button-background-color);
+        color: var(--button-label-color);
+        height: var(--button-height);
+        padding-left: var(--button-padding-x);
+        padding-right: var(--button-padding-x);
+        border-radius: var(--button-border-radius);
+        font-family: var(--button-label-font-family);
+        font-size: var(--button-label-font-size);
+        line-height: var(--button-label-line-height);
+        letter-spacing: var(--button-label-letter-spacing);
+    }
+    .button:hover {
+        background: var(--button-background-color-hover);
+    }
+    .button:active {
+        background: var(--button-background-color-active);
+    }
+    .button:focus {
+        background: var(--button-background-color-focus);
+    }
+    .full-width {
+            width: 100%;
+        } 
+
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,10 @@
 <script>
     import Header from "../lib/Header.svelte";
+    import Button from "../lib/Button.svelte";
 </script>
 
 <h1>Home</h1>
 <p> <a href="about">about</a></p>
 
 <Header />
+<Button class="full-width" label="hello"/>
