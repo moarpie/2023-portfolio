491e62c8b81c2efcdfe75e6b458267e832fb02af
mje <mje@unik.dk>
2023-06-14 16:25:05 +0200

    Created case component and styling

---
+++ src/lib/Case.svelte
@@ -0,0 +1,26 @@
+<script>
+    export let portfolioCases = [];
+    import Button from "$lib/Button.svelte";
+</script>
+
+<div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+    {#each portfolioCases as portfolioCase}
+        <div class="case">
+            <div class="case-image mb-2" style="background-image: {`url(${portfolioCase.image})`}">
+                <!-- <img src= alt={portfolioCase.title} /> -->
+            </div>
+            
+            <h3 class="title-small mb-1">{portfolioCase.title}</h3>
+            <p class="subtitle-small">{portfolioCase.description}</p>
+            <Button link={portfolioCase.link} label="View case" />
+        </div>
+    {/each}
+</div>
+
+<style lang="scss">
+    .case-image {
+        aspect-ratio: 16/9;
+        border-radius: var(--card-border-radius);
+        background-size: cover;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,33 +3,56 @@
     import Button from "$lib/Button.svelte";
     import Card from "$lib/Card.svelte";
     import AboutMe from "$lib/AboutMe.svelte";
+    import Case from "$lib/Case.svelte";
+    let myPortfolioCases = [
+    {
+      image: 'http://placekitten.com/600/200',
+      title: 'Portfolio Case 1',
+      description: 'This is the description for portfolio case 1.',
+      link: 'https://example.com/portfolio-case-1'
+    },
+    {
+      image: 'http://placekitten.com/200/600',
+      title: 'Portfolio Case 2',
+      description: 'This is the description for portfolio case 2.',
+      link: 'https://example.com/portfolio-case-2'
+    }
+  ];
 
 </script>
 
-<h1>Home</h1>
-
 <Header />
+
+
 <p> <a href="about">about</a></p>
+
 <div class="container">
     <div class="section">
         <AboutMe />
-        <Card class="elevation-1">
-            <h1>Card stuff here!</h1>
-        </Card>
-
-        <br><br>
-
-        <br><br>
-        <Card class="elevation-3">
-            <h1>Card stuff here!</h1>
-        </Card>
-            <br><br>
-        <Card class="elevation-4">
-            <h1>Card stuff here!</h1>
-        </Card>
+    </div>
+    <div class="section">
+        <div>
+            <Case portfolioCases={myPortfolioCases} />
+        </div>
     </div>
 </div>
 
+
+<br><br><br><br><br><br><br><br><br><br>
+<Card class="elevation-1">
+    <h1>Card stuff here!</h1>
+</Card>
+
+<br><br>
+
+<br><br>
+<Card class="elevation-3">
+    <h1>Card stuff here!</h1>
+</Card>
+    <br><br>
+<Card class="elevation-4">
+    <h1>Card stuff here!</h1>
+</Card>
 <Button class="full-width" label="This is some text"/>
 
 <style>
