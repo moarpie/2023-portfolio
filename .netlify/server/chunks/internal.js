fb29b989947518258909e749c59d6cef19378df4
mje <mje@unik.dk>
2023-09-04 16:17:37 +0200

    Added logic to hide unfinished cases

--- .netlify/server/chunks/internal.js
+++ .netlify/server/chunks/internal.js
@@ -198,7 +198,7 @@ const options = {
 		<div class="error">
 			<span class="status">` + status + '</span>\n			<div class="message">\n				<h1>' + message + "</h1>\n			</div>\n		</div>\n	</body>\n</html>\n"
   },
-  version_hash: "133f7xz"
+  version_hash: "1t1oxzy"
 };
 function get_hooks() {
   return {};
--- src/lib/templates/Case.svelte
+++ src/lib/templates/Case.svelte
@@ -4,13 +4,19 @@
 </script>
 <div class="grid grid-cols-1 xl:grid-cols-2 gap-x-8 gap-y-16">
     {#each cases as myCase}
-        <div class="case">
-            <div class="case-image mb-2" style="background-image: url('img/cases/thumbnails/{myCase.image}')"></div>
-            
-            <h3 class="title-small mb-1">{myCase.title}</h3>
-            <p class="subtitle-small">{myCase.description}</p>
-            <Button class="primary" link={myCase.link} label={myCase.cta} />
-        </div>
+        
+        {#if myCase.public == true}
+            <div class="case">
+                <div class="case-image mb-2" style="background-image: url('img/cases/thumbnails/{myCase.image}')"></div>
+
+                <h3 class="title-small mb-1">{myCase.title}</h3>
+                <p class="subtitle-small">{myCase.description}</p>
+                <Button class="primary" link={myCase.link} label={myCase.cta} />
+            </div>
+        {:else if myCase.public == false}
+        <!-- Placeholder logic for undisplayed cases -->
+        {/if}
+
     {/each}
 </div>
 
