26e3fa6e958c11639e0ffadbc21092e73e9c3114
mje <mje@unik.dk>
2023-09-05 14:43:18 +0200

    Added handling for 404 with custom page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,22 @@
+<script>
+	import Button from "$lib/components/Button.svelte";
+</script>
+<div class="mt-24 container">
+
+    <div class="flex justify-center">
+
+        <h1 class="display">Page not found 😥</h1>
+
+    </div>
+
+    <div class="mt-16 flex justify-center">
+
+        <p class="subtitle-large">You took a wrong turn at some point, but fear not!</p>
+
+    </div>
+    <div class="mt-16 flex justify-center">
+
+        <Button class="primary" label="Return to safety" link="/"></Button>    
+
+    </div>
+</div>
